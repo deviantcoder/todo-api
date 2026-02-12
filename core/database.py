617811029ba0f42c0b0102f3a8d4dbe3f30791e4
deviantcoder <andrewsafonovpost@gmail.com>
2026-02-12 11:09:35 +0200

    add database config

---
+++ core/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+from core.config import settings
+
+
+engine = create_engine(
+    url=settings.DATABASE_URL,
+    connect_args={'check_same_thread': False}
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False
+)
+
+
+class Base(DeclarativeBase):
+    pass
