c567a4f7db0922631af81e8f7f94023a475445fa
deviantcoder <andrewsafonovpost@gmail.com>
2026-03-29 19:06:09 +0300

    feat: add user model

---
+++ src/models/user.py
@@ -0,0 +1,19 @@
+from sqlalchemy import String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.db.base import Base
+
+
+class User(Base):
+    """
+    User model class
+    """
+
+    __tablename__ = 'users'
+
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False, index=True)
+    email: Mapped[str] = mapped_column(String(200), unique=True, nullable=False, index=True)
+    full_name: Mapped[str] = mapped_column(String(200), nullable=True)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
