36d1ebfe61c23ea6aa0d0ef5bfb98b1fce33e12e
deviantcoder <andrewsafonovpost@gmail.com>
2026-03-29 18:32:59 +0300

    feat: add base model class

---
+++ src/db/base.py
@@ -0,0 +1,19 @@
+import uuid
+from datetime import datetime
+
+from sqlalchemy import UUID, DateTime, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    """
+    Base model class with common attributes
+    """
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True, index=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), default=func.now(), nullable=False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), default=func.now(), onupdate=func.now(), nullable=False
+    )
