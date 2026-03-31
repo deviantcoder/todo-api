48155b0129a2c1130cafe73d110f797d96ffcbc5
deviantcoder <andrewsafonovpost@gmail.com>
2026-03-31 11:50:27 +0300

    feat(task): add task CRUD routes

--- src/api/v1/router.py
+++ src/api/v1/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from .routes import auth
+from .routes import auth, task
 
 
 v1_router = APIRouter(prefix='/v1')
 
 v1_router.include_router(auth.router)
+v1_router.include_router(task.router)
---
+++ src/api/v1/routes/task.py
@@ -0,0 +1,56 @@
+from uuid import UUID
+
+from fastapi import APIRouter, status
+
+from src.api.deps.task import TaskServiceDep
+from src.api.deps.auth import CurrentUserDep
+from src.models.task import Task
+from src.schemas.task import TaskResponse, TaskCreate, TaskUpdate
+
+
+router = APIRouter(prefix='/tasks', tags=['tasks'])
+
+
+@router.get('/', response_model=list[TaskResponse])
+async def get_tasks(
+    service: TaskServiceDep,
+    current_user: CurrentUserDep
+) -> list[Task]:
+    return await service.get_all(current_user)
+
+
+@router.get('/{task_id}', response_model=TaskResponse)
+async def get_task(
+    service: TaskServiceDep,
+    task_id: UUID,
+    current_user: CurrentUserDep
+) -> Task:
+    return await service.get_by_id(task_id, current_user)
+
+
+@router.post('/', response_model=TaskResponse)
+async def create_task(
+    service: TaskServiceDep,
+    data: TaskCreate,
+    current_user: CurrentUserDep
+) -> Task:
+    return await service.create(data, current_user)
+
+
+@router.patch('/{task_id}', response_model=TaskResponse)
+async def update_task(
+    service: TaskServiceDep,
+    task_id: UUID,
+    data: TaskUpdate,
+    current_user: CurrentUserDep
+):
+    return await service.update(task_id, data, current_user)
+
+
+@router.delete('/{task_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_task(
+    service: TaskServiceDep,
+    task_id: UUID,
+    current_user: CurrentUserDep
+) -> None:
+    return await service.delete(task_id, current_user)
