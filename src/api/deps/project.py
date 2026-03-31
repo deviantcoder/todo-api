64329060106d71fe1468f2ded53f199fc9c49345
deviantcoder <andrewsafonovpost@gmail.com>
2026-03-31 18:34:17 +0300

    feat(project): add project dependencies

---
+++ src/api/deps/project.py
@@ -0,0 +1,21 @@
+from typing import Annotated
+
+from fastapi import Depends
+
+from src.api.deps.session import SessionDep
+from src.repos.project import ProjectRepository
+from src.services.project import ProjectService
+
+
+def get_project_repo(session: SessionDep) -> ProjectRepository:
+    return ProjectRepository(session)
+
+
+ProjectRepoDep = Annotated[ProjectRepository, Depends(get_project_repo)]
+
+
+def get_project_service(repo: ProjectRepoDep) -> ProjectService:
+    return ProjectService(repo)
+
+
+ProjectServiceDep = Annotated[ProjectService, Depends(get_project_service)]
