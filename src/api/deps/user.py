d6c708dc8976ff414728d6a79dd74aca183cf427
deviantcoder <andrewsafonovpost@gmail.com>
2026-04-02 09:50:57 +0300

    feat(user): add user service dependency

--- src/api/deps/user.py
+++ src/api/deps/user.py
@@ -4,6 +4,7 @@ from fastapi import Depends
 
 from src.api.deps.session import SessionDep
 from src.repos.user import UserRepository
+from src.services.user import UserService
 
 
 def get_user_repo(session: SessionDep) -> UserRepository:
@@ -11,3 +12,10 @@ def get_user_repo(session: SessionDep) -> UserRepository:
 
 
 UserRepoDep = Annotated[UserRepository, Depends(get_user_repo)]
+
+
+def get_user_service(repo: UserRepoDep) -> UserService:
+    return UserService(repo)
+
+
+UserServiceDep = Annotated[UserService, Depends(get_user_service)]
